8b521adf58bfe23477e0567e6e1b8c36822fa8f0
Simon Tarasenko <semeontarasenko@yandex.ru>
2023-07-08 17:48:00 +0300

    Исправил ошибку в автотестах

--- controllers/users.js
+++ controllers/users.js
@@ -41,7 +41,13 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.send(user))
+    .then((user) => res.send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+      password: user.password,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы невалидные данные'));
